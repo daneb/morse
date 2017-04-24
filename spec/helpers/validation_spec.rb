ff89beb67d7cb1f90d00bc12088eaa37cc285c7d
daneb <dane.balia@hetzner.co.za>
2017-04-24 23:22:15 +0200

    Added coverage for validation

---
+++ spec/helpers/validation_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe Morse::Helpers::Validation do
+  MH = Morse::Helpers
+
+  it 'determines if there are non-alphanumerics in a string' do
+    content = 'Symphony No.9, is a rather fantastical beast'
+    expect(MH::Validation.contains_non_alphanumeric?(content)).to eq true
+  end
+
+  it 'does not detect non-alphanumerics when there is none' do
+    content = 'Symphony Number 9 is a rather fantastical beast'
+    expect(MH::Validation.contains_non_alphanumeric?(content)).to eq false
+  end
+end
